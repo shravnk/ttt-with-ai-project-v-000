97a0369c9e8aa66523ccee776ae17c2dda2638e1
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-22 18:19:41 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -50,6 +50,6 @@ end
 
 robot = Players::Computer.new("X")
 test_board = Board.new
-test_board.cells = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+test_board.cells = ["X", " ", " ", " ", "X", " ", " ", " ", " "]
 t = robot.move(test_board)
 binding.pry

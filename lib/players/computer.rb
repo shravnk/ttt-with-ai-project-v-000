f468522b7659361ec9f5413298cd2f483b664231
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-22 18:25:07 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -13,7 +13,7 @@ module Players
       elsif !board.taken?("5")
         return "5"
       else
-        for i in Random.new.rand(1..9) do
+        rand(1..9).each do |i|
           if !board.taken?(i.to_s)
             return i.to_s
             break
